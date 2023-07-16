ca1902d8840750a66346b3b3fea1aac0a3345f9b
Nasir Zilan <devzil589@gmail.com>
2023-07-16 14:02:57 +0600

    5th commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,19 @@
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 function Home(){
     return (
         <>
         <Navbar/>
-        <h1>This is Home</h1>
+        <Hero
+        cName = 'hero'
+        heroImg = 'https://burst.shopifycdn.com/photos/top-of-tower.jpg?width=373&format=pjpg&exif=1&iptc=1'
+        title='Your Journey Your story'
+        text='Choose your favourite destination'
+        buttonText='Travel Plan'
+        url='/'
+        btnClass='show'
+        />
         </>
     )
 }
