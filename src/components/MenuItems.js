67b446fb6f2d7edad19deb9f611965c79be11080
Nasir Zilan <devzil589@gmail.com>
2023-07-16 10:50:07 +0600

    2nd commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,37 @@
+export const MenuItems = [
+    {
+        title:"Home",
+        url:"/",
+        cName:'nav-links',
+        icon:'fa-solid fa-house-user',
+    },
+    {
+        title:"About",
+        url:"/about",
+        cName:'nav-links',
+        icon:'fa-sharp fa-regular fa-circle',
+    },
+    {
+        title:"Service",
+        url:"/service",
+        cName:'nav-links',
+        icon:'fa-solid fa-briefcase',
+    },
+    {
+        title:"Contact",
+        url:"/contact",
+        cName:'nav-links',
+        icon:'fa-solid fa-house-user',
+    },
+    {
+        title:"Contact",
+        url:"/contact",
+        cName:'nav-links',
+        icon:"fa-solid fa-address-book",
+    },
+    {
+        title:"Sign Up",
+        url:"/signup",
+        cName:'nav-links-mobile',
+    },
+]
